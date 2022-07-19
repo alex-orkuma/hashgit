a0e6f54691f9c8f6f528bd42d1891afd3852893a
alex-orkuma <orkumashi@gmail.com>
2022-07-19 21:18:51 +0100

    Revert "commiting from visual studio"
    This reverts commit 7a67687ec1ff05c0e446a06bd236e0a4ff26e15c.

--- PacktLibrary/GenericThing.cs
+++ PacktLibrary/GenericThing.cs
@@ -4,19 +4,7 @@ namespace Packt.Shared
 {
     public class GenericThing<T> where T : IComparable
     {
-        public T Data = default(T);
 
-        public string Process(T input)
-        {
-            if (Data.CompareTo(input) == 0)
-            {
-                return "Data and Input are the same";
-            }
-            else
-            {
-                return "Data and Input are NOT the same";
-            }
-        }
     }
 }
 
